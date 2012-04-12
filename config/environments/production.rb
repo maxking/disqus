4a14c1e916b88188939a2a9009579a93b92e939c
Abhilash Raj <raj.abhilash1@gmail.com>
2012-04-13 02:51:35 +0530

    revert changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Disqus::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = True
-  config.action_controller.perform_caching = False
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
