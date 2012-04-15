b0f6f1e1908178152b2be947a62d9d262fae68c3
Abhilash Raj <raj.abhilash1@gmail.com>
2012-04-15 20:06:57 +0530

    changed

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,7 +1,7 @@
 class Startup < ActiveRecord::Base
 	has_many :comments
 
-	def self.rake_create(startup_id,project_id, company_name, email)
-		Startup.create(:id => 'startup_id' ,:project_id => 'project_id', :name => 'company_name', :email => 'email')
+	def self.rake_create(startup_id , project_id, company_name, email)
+		Startup.create(:id => startup_id ,:project_id => project_id, :name => company_name, :email => email)
 	end
 end
